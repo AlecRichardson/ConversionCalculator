cea4a7438dc53667524eec8d291c5e6ca2a313cb
Alec Richardson <alec.richardson@eventbrite.com>
2019-05-02 11:20:00 -0500

    Added conversion functionality to UI

--- ConversionCalculator/ConversionCalculatorViewController.swift
+++ ConversionCalculator/ConversionCalculatorViewController.swift
@@ -14,9 +14,20 @@ class ConversionCalculatorViewController: UIViewController {
         var label: String
         var inputUnit: String
         var outputUnit: String
+        var type: String
     }
     
-    var displays = [display(label: "fahrenheit to celsius", inputUnit: "°F", outputUnit: "°C"), display(label: "celsius to fahrenheit", inputUnit: "°C", outputUnit: "°F"), display(label: "miles to kilometers", inputUnit: "mi", outputUnit: "km"), display(label: "kilometers to miles", inputUnit: "km", outputUnit: "mi")]
+    var displays = [
+        display(label: "fahrenheit to celsius", inputUnit: "°F", outputUnit: "°C", type: "FtoC"),
+        display(label: "celsius to fahrenheit", inputUnit: "°C", outputUnit: "°F", type: "CtoF"),
+        display(label: "miles to kilometers", inputUnit: "mi", outputUnit: "km", type: "MtoK"),
+        display(label: "kilometers to miles", inputUnit: "km", outputUnit: "mi", type: "KtoM")
+    ]
+    
+    var currentDisplay: display? = nil
+    var input: String = ""
+    var negativeNumber: Bool = false
+    var decimal: Bool = false
 
     @IBOutlet weak var outputDisplay: UITextField!
     @IBOutlet weak var inputDisplay: UITextField!
@@ -25,33 +36,100 @@ class ConversionCalculatorViewController: UIViewController {
         super.viewDidLoad()
         self.inputDisplay.text = displays[0].inputUnit
         self.outputDisplay.text = displays[0].outputUnit
+        self.currentDisplay = displays[0]
         // Do any additional setup after loading the view.
     }
     
+    func convert() {
+        let floatInput = Float(input)
+        var conversion: Float? = nil
+        var error: Bool = false
+        
+        switch currentDisplay?.type {
+            case "FtoC": conversion = (floatInput! - 32) * (5/9)
+            case "CtoF": conversion = (floatInput! * (9/5)) + 32
+            case "MtoK": conversion = (floatInput! / 0.62137)
+            case "KtoM": conversion = (floatInput! * 0.62137)
+            default: error = true
+        }
+        
+        if (error) {
+            outputDisplay.text = "Error"
+        }
+        
+        inputDisplay.text = input + (currentDisplay?.inputUnit)!
+        outputDisplay.text = String(conversion!) + (currentDisplay?.outputUnit)!
+    }
    
+    @IBAction func numbers(_ sender: UIButton) {
+        input += String(sender.tag)
+        convert()
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+        clearFunc()
+    }
+    
+    func clearFunc() {
+        self.inputDisplay.text = (currentDisplay?.inputUnit)!
+        self.outputDisplay.text = (currentDisplay?.outputUnit)!
+        input = ""
+        negativeNumber = false
+        decimal = false
+    }
+    
+    @IBAction func posNegButton(_ sender: Any) {
+        if (input != "") {
+            if (negativeNumber) {
+                negativeNumber = false
+                input = input.filter { "0123456789.".contains($0) }
+            } else {
+                negativeNumber = true
+                input = "-" + input
+            }
+        }
+
+        convert()
+    }
+    
+    @IBAction func decimalButton(_ sender: Any) {
+        if (!decimal) {
+            decimal = true
+            input = input + "."
+            convert()
+        }
+    }
+    
     @IBAction func convert(_ sender: UIButton) {
         let alert = UIAlertController(title: "Choose Converter", message: "Choose!", preferredStyle: UIAlertController.Style.actionSheet)
         alert.addAction(UIAlertAction(title: "fahrenheit to celsius", style: UIAlertAction.Style.default, handler: {
             (alertAction) -> Void in
+            self.clearFunc()
             self.inputDisplay.text = self.displays[0].inputUnit
             self.outputDisplay.text = self.displays[0].outputUnit
+            self.currentDisplay = self.displays[0]
             
         }))
         alert.addAction(UIAlertAction(title: "celsius to fahrenheit", style: UIAlertAction.Style.default, handler: {
             (alertAction) -> Void in
+            self.clearFunc()
             self.inputDisplay.text = self.displays[1].inputUnit
             self.outputDisplay.text = self.displays[1].outputUnit
-            
+            self.currentDisplay = self.displays[1]
         }))
         alert.addAction(UIAlertAction(title: "miles to kilometers", style: UIAlertAction.Style.default, handler: {
             (alertAction) -> Void in
+            self.clearFunc()
             self.inputDisplay.text = self.displays[2].inputUnit
             self.outputDisplay.text = self.displays[2].outputUnit
+            self.currentDisplay = self.displays[2]
         }))
         alert.addAction(UIAlertAction(title: "kilometers to miles", style: UIAlertAction.Style.default, handler: {
             (alertAction) -> Void in
+            self.clearFunc()
             self.inputDisplay.text = self.displays[3].inputUnit
             self.outputDisplay.text = self.displays[3].outputUnit
+            self.currentDisplay = self.displays[3]
         }))
         self.present(alert, animated: true, completion: nil)
     }
